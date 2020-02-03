9db3ccbd1b09bc53d612357b83d5aeea857c40e9
Boran Seckin <me@boranseckin.com>
2020-02-03 13:35:53 -0500

    Add comments to client.js

--- client.js
+++ client.js
@@ -1,16 +1,25 @@
 #!/usr/bin/env node
 
+/**
+ * SCS - Client
+ * *
+ * This code is used to connect to a Socket.io server created by SCS-Server.
+ */
+
 const readline = require('readline');
 const socketio = require('socket.io-client');
 const color = require('ansi-color').set;
 
+// All these variables are used to keep a local reference.
 let id;
 let username;
 let server;
 let port;
 let channel;
 
-let socket;
+let socket; // Main socket connection
+
+// Connection options.
 const opts = {
     reconnection: true,
     reconnectionDelay: 500,
@@ -19,11 +28,17 @@ const opts = {
     forceNew: true,
 };
 
+// Counter to handle instant exit when pressed 'ctrl-c' twice.
 let sigintCount = 0;
 
+// Create readline interface.
 const rl = readline.createInterface(process.stdin, process.stdout);
 
 function consoleOut(msg) {
+    /**
+     * Handle console.log while the readline interface is on.
+     */
+
     process.stdout.clearLine();
     process.stdout.cursorTo(0);
     console.log(msg);
@@ -31,12 +46,35 @@ function consoleOut(msg) {
 }
 
 function updateSocket(newSocket) {
+    /**
+     * When the socket changes (reconnection, channel change), update the reference object.
+     */
+
+    // Change the socket object.
     socket = newSocket;
+    // Change the local ID.
     id = newSocket.id;
+    // Initialize chat again to refresh the listeners.
     chat();
 }
 
 function parseAddress(address, callback) {
+    /**
+     * Parse the address input when the client is first initialized.
+     * Get address (URL), port and channel from the input.
+     * If any information is missing, assume default values.
+     * *
+     * Default Address: (localhost:3636/)
+     * *
+     * The port is specified after a colon (:).
+     * The channel is specified after a forward slash (/).
+     * *
+     * Example: (localhost:3636/test)
+     *  - Address: "localhost"
+     *  - Port: "3636"
+     *  - Channel: "test"
+     */
+
     if (address) {
         if (address.includes(':')) {
             server = address.slice(0, address.indexOf(':'));
@@ -58,31 +96,44 @@ function parseAddress(address, callback) {
         port = '3636';
         channel = '';
     }
+
+    // Call callback.
     callback();
 }
 
+// Ask user to input the server address.
 rl.question('Server: ', (address) => {
+    // Parse the address.
     parseAddress(address, () => {
+        // Connect to the specified address.
         socket = socketio.connect(`http://${server}:${port}/${channel}`, opts);
     });
 
+    // When connected;
     socket.once('connect', () => {
+        // Update the local user ID value.
         id = socket.id;
         consoleOut(color(`[!] Connected to ${socket.io.uri}`, 'yellow'));
 
+        // Ask user to input a username.
         rl.question('Username: ', (input) => {
+            // The input cannot be empty.
             if (input) {
+                // Update the local username value.
                 username = input;
+                // Send a handshake request, indicating that this is the initial connection.
                 socket.emit('handshake', { first: true, username });
             } else {
                 consoleOut(color('Username cannot be empty!', 'red'));
-                process.exit();
+                process.exit(); // Terminate the program.
             }
         });
     });
 
+    // When handshake response arrives;
     socket.once('handshake', (data) => {
         if (data.type !== 'confirm') {
+            // If the server does not send a confirmation terminate the program.
             consoleOut(color('Handshake with the server was unsuccessful!', 'red'));
             if (data.reason) {
                 consoleOut(color(`Reason: ${data.reason}`, 'red'));
@@ -90,43 +141,60 @@ rl.question('Server: ', (address) => {
             return process.exit();
         }
 
+        // Initialize the chat.
         chat();
         return rl.prompt();
     });
 
+    // If an error occurs during connection;
     socket.once('connect_error', () => {
         consoleOut(color('Cannot connect to that server!', 'red'));
         return process.exit();
     });
 
+    // If an error occurs in general;
     socket.once('error', () => {
         consoleOut(color('Cannot connect to that server!', 'red'));
         return process.exit();
     });
 });
 
+// Handle chat input from the user.
 rl.on('line', (input) => {
+    // Trim any space before or after the input.
     const line = input.trim();
+
+    // If the input start with a forward slash assume command.
     if (line[0] === '/' && line.length > 1) {
+        // Take the first string group without the forward slash as the command.
         const cmd = line.split(' ')[0].slice(1);
+        // Take the rest as the argument.
         const arg = line.slice(line.split(' ')[0].length + 1, line.length);
+        // Call chatCommand.
         chatCommands(cmd, arg);
     } else {
+    // Otherwise send it as a chat message.
         socket.emit('send', { type: 'chat', message: line, username });
         rl.prompt();
     }
 });
 
+// Handle 'ctrl-c' input during the program.
 rl.on('SIGINT', () => {
+    // When called increase the counter.
     sigintCount += 1;
 
     if (sigintCount > 1) {
+        // If SIGINT is called more than once exit immediately.
         process.exit();
     }
 
+    // Ask if the user wants to exit.
     rl.question('Are you sure you want to exit? (y/n): ', (answer) => {
+        // If the answer is yes (or another iteration of it), exit.
         if (answer.match(/^y(es)?$/i)) process.exit();
 
+        // Otherwise reset the counter and continue.
         sigintCount = 0;
         return rl.prompt();
     });
