dbdc72471a97da27533bdd238e98a55234efdab8
Boran Seckin <me@boranseckin.com>
2020-02-09 08:58:55 -0500

    Add comments to client.js

--- client.js
+++ client.js
@@ -201,13 +201,21 @@ rl.on('SIGINT', () => {
 });
 
 function chatCommands(cmd, arg = '') {
+    /**
+     * Parse and handle '/' commands.
+     */
+
     let message = '';
     let to = '';
 
     switch (cmd) {
     case 'username':
+        // Change username
         if (arg) {
+            // Send change request for the username.
             socket.emit('username', { username: arg });
+
+            // Except a confirmation from the server.
             socket.once('username', (data) => {
                 if (data.type === 'confirm') {
                     username = data.username;
@@ -218,19 +226,25 @@ function chatCommands(cmd, arg = '') {
         break;
 
     case 'w':
-        to = arg.split(' ')[0];
-        message = arg.substr(to.length, arg.length);
+        // Whisper another user.
+        to = arg.split(' ')[0]; // Get the destination.
+        message = arg.substr(to.length, arg.length); // Get the message.
+        // Send the message.
         socket.emit('whisper', { from: username, to, message });
         rl.prompt();
         break;
 
     case 'me':
+        // Emote to the whole room.
+        // If there is no message just show the username.
         message = arg ? `[${username}] ${arg}` : `[${username}]`;
+        // Send the message.
         socket.emit('send', { type: 'emote', message });
         rl.prompt();
         break;
 
     case 'ch':
+        // Used to change the channel. Currently DISABLED.
         /*
         console_out(color(`Leaving the channel [/${channel}]`, 'yellow'));
         socket.disconnect();
@@ -248,79 +262,106 @@ function chatCommands(cmd, arg = '') {
         break;
 
     case 'room':
+        // Change the room.
         socket.emit('room', { room: arg });
         rl.prompt();
         break;
 
     case 'clear':
+        // Clear the chat.
         process.stdout.write('\u001B[2J\u001B[0;0f');
         rl.prompt();
         break;
 
     case 'id':
+        // Print the user's ID.
         consoleOut(color('ID: ', 'yellow') + id);
         break;
 
     case 'server':
+        // Print the server's URL.
         consoleOut(color('URL: ', 'yellow') + socket.io.uri);
         break;
 
     case 'status':
+        // Show the connection status.
         consoleOut(color('State: ', 'yellow') + socket.io.readyState);
         break;
 
     case 'users':
+        // List all online users in the server.
         socket.emit('list_users');
         rl.prompt();
         break;
 
     case 'exit':
+        // Exit the program.
         process.exit();
         break;
 
     default:
+        // Unknown Command.
         consoleOut(color('[!] Unknown command!', 'red'));
         break;
     }
 }
 
 function chat() {
+    /**
+     * Handle incomming messages and connections.
+     */
+
+    // To reset all listeners, first remove the old ones.
     socket.removeAllListeners();
 
+    // Handle incoming message and print with format.
     socket.on('message', (data) => {
         if (data.type === 'chat' && data.username !== username) {
+            // Default chat.
             const prefix = color(`<${data.username}> `, 'green');
             consoleOut(prefix + data.message);
         } else if (data.type === 'notice') {
+            // Notice message.
             consoleOut(color(data.message, 'cyan'));
         } else if (data.type === 'tell' && data.to === username) {
+            // Whisper message.
             const prefix = color(`[${data.from} -> ${data.to}]`, 'magenta');
             consoleOut(prefix + data.message);
         } else if (data.type === 'emote') {
+            // Emote message.
             consoleOut(color(data.message, 'underline'));
         }
     });
 
+    // If user disconnects from the server
     socket.once('disconnect', (reason) => {
+        // If it was intentional, do nothing.
         if (reason === 'io client disconnect') return;
 
+        // Indicate the lost connection.
         consoleOut(color('Connection lost!', 'red'));
+        // Stop the chat listener.
         socket.removeListener('message');
 
+        // Try to reconnect.
         socket = socketio.connect(`http://${server}:${port}/${channel}`, opts);
         socket.connect();
     });
 
+    // If user reconnects to the server
     socket.once('reconnect', () => {
+        // Send a handshake for reconnection.
         socket.emit('handshake', { first: false, username });
 
         socket.once('handshake', (data) => {
             if (data.type !== 'confirm') {
+                // If handshake is not complete, exit.
                 consoleOut(color('Handshake with the server was unsuccessful!', 'red'));
                 rl.close();
                 return process.exit();
             }
 
+            // If handshake is complete, update sockets.
             updateSocket(socket);
             consoleOut(color('Reconnected to the server!', 'green'));
             return rl.prompt();
